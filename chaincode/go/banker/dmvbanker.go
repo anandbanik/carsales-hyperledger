d569b1f8060436e1ea6127f1f8604a5f87d579e8
Anand Banik <anandbanik@anands-mbp.attlocal.net>
2018-01-19 01:48:30 -0600

    added role for banker and dmv users

--- chaincode/go/banker/dmvbanker.go
+++ chaincode/go/banker/dmvbanker.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	pb "github.com/hyperledger/fabric/protos/peer"
-	"strings"
-	"encoding/pem"
 	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"fmt"
 	"strconv"
+	"strings"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 	//"bytes"
 )
 
@@ -18,15 +19,15 @@ type DmvDealerChaincode struct {
 }
 
 type Loan struct {
-	LoanId string `json:"loanId"`
-	VinNumber string `json:"vin"`
-	Amount int `json:"amount"`
-	SsnNumber string `json:"ssnNumber"`
-	LoanPeriod int `json:"loanPeriod"`
-	Apr int `json:"apr"`
-	MonthlyPayment int `json:"monthlyPayment"`
-	Status string `json:"status"`
-	Org string `json:"org"`
+	LoanId         string `json:"loanId"`
+	VinNumber      string `json:"vin"`
+	Amount         int    `json:"amount"`
+	SsnNumber      string `json:"ssnNumber"`
+	LoanPeriod     int    `json:"loanPeriod"`
+	Apr            int    `json:"apr"`
+	MonthlyPayment int    `json:"monthlyPayment"`
+	Status         string `json:"status"`
+	Org            string `json:"org"`
 }
 
 func (t *DmvDealerChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
@@ -39,17 +40,13 @@ func (t *DmvDealerChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Respons
 	function, args := stub.GetFunctionAndParameters()
 	if function == "loan" {
 		return t.loan(stub, args)
-	}else if function == "query" {
+	} else if function == "query" {
 		return t.query(stub, args)
 	}
 
-	return pb.Response{Status:403, Message:"unknown function name"}
+	return pb.Response{Status: 403, Message: "unknown function name"}
 }
 func (t *DmvDealerChaincode) loan(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-
-	if len(args) != 6 {
-		return pb.Response{Status:403, Message:"incorrect number of arguments"}
-	}
 
 	creatorBytes, err := stub.GetCreator()
 	if err != nil {
@@ -57,53 +54,98 @@ func (t *DmvDealerChaincode) loan(stub shim.ChaincodeStubInterface, args []strin
 	}
 
 	ssn, org := getCreator(creatorBytes)
-	if org != "" {
+	if org == "" {
 		logger.Debug("Org is null")
-	}
+		return shim.Error("cannot get Org")
+	} else if org == "dmv" {
 
-	amount, err := strconv.Atoi(args[2])
-	if(err != nil){
-		return shim.Error("Invalid amount, expecting a integer value")
-	}
+		if len(args) != 6 {
+			return pb.Response{Status: 403, Message: "incorrect number of arguments"}
+		}
 
-	loanPeriod, err := strconv.Atoi(args[3])
-	if(err != nil){
-		return shim.Error("Invalid Loan amount, expecting a integer value")
-	}
+		amount, err := strconv.Atoi(args[2])
+		if err != nil {
+			return shim.Error("Invalid amount, expecting a integer value")
+		}
 
-	interest, err := strconv.Atoi(args[4])
-	if(err != nil){
-		return shim.Error("Invalid APR amount, expecting a integer value")
-	}
+		loanPeriod, err := strconv.Atoi(args[3])
+		if err != nil {
+			return shim.Error("Invalid Loan amount, expecting a integer value")
+		}
 
-	monthlyPayment, err := strconv.Atoi(args[5])
-	if(err != nil){
-		return shim.Error("Invalid Monthly Payment amount, expecting a integer value")
-	}
+		interest, err := strconv.Atoi(args[4])
+		if err != nil {
+			return shim.Error("Invalid APR amount, expecting a integer value")
+		}
 
-	loanObj := &Loan{
-				LoanId: args[0],
-				VinNumber: args[1],
-				Amount: amount,
-				SsnNumber: ssn,
-				LoanPeriod: loanPeriod,
-				Apr: interest,
-				MonthlyPayment: monthlyPayment,
-				Org: org}
+		monthlyPayment, err := strconv.Atoi(args[5])
+		if err != nil {
+			return shim.Error("Invalid Monthly Payment amount, expecting a integer value")
+		}
 
-	jsonLoanObj, err := json.Marshal(loanObj)	
-	if(err != nil){
-		return shim.Error("Cannot create Json Object")
-	}
+		status := "Applied"
 
-	logger.Debug("Json Obj: "+string(jsonLoanObj))
+		loanObj := &Loan{
+			LoanId:         args[0],
+			VinNumber:      args[1],
+			Amount:         amount,
+			SsnNumber:      ssn,
+			LoanPeriod:     loanPeriod,
+			Apr:            interest,
+			MonthlyPayment: monthlyPayment,
+			Org:            org,
+			Status:         status}
 
-	key := ssn+"@"+args[1]
+		jsonLoanObj, err := json.Marshal(loanObj)
+		if err != nil {
+			return shim.Error("Cannot create Json Object")
+		}
 
-	err = stub.PutState(key, jsonLoanObj)
+		logger.Debug("Json Obj: " + string(jsonLoanObj))
 
-	if err != nil {
-		return shim.Error("cannot put state")
+		key := ssn + "@" + args[1]
+
+		err = stub.PutState(key, jsonLoanObj)
+
+		if err != nil {
+			return shim.Error("cannot put state")
+		}
+
+		logger.Debug("Loan Object added")
+
+	} else if org == "banker" {
+
+		if len(args) != 3 {
+			return pb.Response{Status: 403, Message: "incorrect number of arguments"}
+		}
+
+		key := args[0] + "@" + args[1]
+
+		loanBytes, err := stub.GetState(key)
+
+		if err != nil {
+			return shim.Error("cannot get state")
+		} else if loanBytes == nil {
+			return shim.Error("Cannot get loan object")
+		}
+
+		var loanObj Loan
+		errUnmarshal := json.Unmarshal([]byte(loanBytes), &loanObj)
+		if errUnmarshal != nil {
+			return shim.Error("Cannot unmarshal Loan Object")
+		}
+
+		logger.Debug("Loan Object: " + string(loanBytes))
+
+		loanObj.Status = args[2]
+
+		loanObjBytes, _ := json.Marshal(loanObj)
+
+		errLoan := stub.PutState(key, loanObjBytes)
+		if errLoan != nil {
+			return shim.Error("Error updating Loan Object: " + err.Error())
+		}
+		logger.Info("Update sucessfull")
 	}
 
 	return shim.Success(nil)
@@ -111,7 +153,7 @@ func (t *DmvDealerChaincode) loan(stub shim.ChaincodeStubInterface, args []strin
 
 func (t *DmvDealerChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
-		return pb.Response{Status:403, Message:"incorrect number of arguments"}
+		return pb.Response{Status: 403, Message: "incorrect number of arguments"}
 	}
 
 	if args[0] == "health" {
@@ -126,29 +168,37 @@ func (t *DmvDealerChaincode) query(stub shim.ChaincodeStubInterface, args []stri
 
 	ssn, org := getCreator(creatorBytes)
 
-	if org != ""{
+	if org == "" {
 		logger.Debug("Org is null")
-	}
-	
-	key := ssn+"@"+args[0]
+	} else if org == "dmv" {
+		key := ssn + "@" + args[0]
 
-	bytes, err := stub.GetState(key)
-	if err != nil {
-		return shim.Error("cannot get state")
+		bytes, err := stub.GetState(key)
+		if err != nil {
+			return shim.Error("cannot get state")
+		}
+		return shim.Success(bytes)
+	} else if org == "banker" {
+
+		key := args[0] + "@" + args[1]
+
+		bytes, err := stub.GetState(key)
+		if err != nil {
+			return shim.Error("cannot get state")
+		}
+		return shim.Success(bytes)
 	}
 
-	return shim.Success(bytes)
+	return shim.Success(nil)
 }
 
-
-var getCreator = func (certificate []byte) (string, string) {
-	data := certificate[strings.Index(string(certificate), "-----"): strings.LastIndex(string(certificate), "-----")+5]
+var getCreator = func(certificate []byte) (string, string) {
+	data := certificate[strings.Index(string(certificate), "-----") : strings.LastIndex(string(certificate), "-----")+5]
 	block, _ := pem.Decode([]byte(data))
 	cert, _ := x509.ParseCertificate(block.Bytes)
 	organization := cert.Issuer.Organization[0]
 	commonName := cert.Subject.CommonName
 	logger.Debug("commonName: " + commonName + ", organization: " + organization)
-
 
 	organizationShort := strings.Split(organization, ".")[0]
 
